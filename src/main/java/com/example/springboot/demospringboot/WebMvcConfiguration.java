6361e5838afb8cb6a66f14a445f0753cac571826
Emiliano Saenz <32196924+saenzemiliano@users.noreply.github.com>
2018-10-31 13:51:47 -0300

    WebSecurityConfig was implemented

--- src/main/java/com/example/springboot/demospringboot/WebMvcConfiguration.java
+++ src/main/java/com/example/springboot/demospringboot/WebMvcConfiguration.java
@@ -1,14 +1,17 @@
 package com.example.springboot.demospringboot;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        
         registry.addViewController("/page/home").setViewName("home");
         registry.addViewController("/page/hello").setViewName("hello");
         registry.addViewController("/page/public/other").setViewName("public/other");
@@ -20,6 +23,8 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addRedirectViewController("/page", "/page/home");
         registry.addRedirectViewController("/page/public/", "/page/home");
         registry.addRedirectViewController("/page/", "/page/home");
+        registry.addRedirectViewController("/login", "/page/login");
+        registry.addRedirectViewController("/login/", "/page/login");
     }
 
 }
