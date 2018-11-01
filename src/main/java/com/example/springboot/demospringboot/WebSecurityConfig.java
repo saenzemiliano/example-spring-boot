b46423b0afb0f40ecfdb2dfb84035384b4c984e9
Emiliano Saenz <32196924+saenzemiliano@users.noreply.github.com>
2018-11-01 11:19:35 -0300

    JdbcUserDetailsManager was enabled

--- src/main/java/com/example/springboot/demospringboot/WebSecurityConfig.java
+++ src/main/java/com/example/springboot/demospringboot/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springboot.demospringboot;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,52 +12,65 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+                .authorizeRequests()
                 .antMatchers("/", "/resources/**", "/page/public/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-            .formLogin()
+                .formLogin()
                 .loginPage("/page/login")
                 .permitAll()
                 .and()
-            .logout()
+                .logout()
                 .permitAll();
     }
+
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(dataSource);
+
+         UserDetails user =
+                User.withUsername("user")
+                .password("password")
+                .roles("USER", "ADMIN", "OPERATOR")
+                .passwordEncoder(x -> new BCryptPasswordEncoder().encode(x))
+                .build();
+        
+        if (!jdbcUserDetailsManager.userExists(user.getUsername())) {
+            jdbcUserDetailsManager.createUser(user);
+        } else {
+            jdbcUserDetailsManager.updateUser(user);
+        }
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
     
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
 
 //    @Bean
 //    @Override
 //    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withUsername("uesr")
-//                .password("pass")
-//                .roles("USER")
-//                .passwordEncoder(x -> new BCryptPasswordEncoder().encode(x))
-//                .build();
+//        UserDetails user
+//                = User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
 //        return new InMemoryUserDetailsManager(user);
 //    }
-    
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-             User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user);
-    }
 }
---
+++ src/main/java/com/example/springboot/demospringboot/model/GroupAuthorities.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.springboot.demospringboot.model;
+
+import java.io.Serializable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author esaenz
+ */
+@Entity
+@Table(name = "GROUP_AUTHORITIES")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "GroupAuthorities.findAll", query = "SELECT g FROM GroupAuthorities g")
+    , @NamedQuery(name = "GroupAuthorities.findByGroupId", query = "SELECT g FROM GroupAuthorities g WHERE g.groupAuthoritiesPK.groupId = :groupId")
+    , @NamedQuery(name = "GroupAuthorities.findByAuthority", query = "SELECT g FROM GroupAuthorities g WHERE g.groupAuthoritiesPK.authority = :authority")})
+public class GroupAuthorities implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @EmbeddedId
+    protected GroupAuthoritiesPK groupAuthoritiesPK;
+    @JoinColumn(name = "GROUP_ID", referencedColumnName = "GROUP_ID", insertable = false, updatable = false)
+    @ManyToOne(optional = false)
+    private Groups groups;
+
+    public GroupAuthorities() {
+    }
+
+    public GroupAuthorities(GroupAuthoritiesPK groupAuthoritiesPK) {
+        this.groupAuthoritiesPK = groupAuthoritiesPK;
+    }
+
+    public GroupAuthorities(int groupId, String authority) {
+        this.groupAuthoritiesPK = new GroupAuthoritiesPK(groupId, authority);
+    }
+
+    public GroupAuthoritiesPK getGroupAuthoritiesPK() {
+        return groupAuthoritiesPK;
+    }
+
+    public void setGroupAuthoritiesPK(GroupAuthoritiesPK groupAuthoritiesPK) {
+        this.groupAuthoritiesPK = groupAuthoritiesPK;
+    }
+
+    public Groups getGroups() {
+        return groups;
+    }
+
+    public void setGroups(Groups groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (groupAuthoritiesPK != null ? groupAuthoritiesPK.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof GroupAuthorities)) {
+            return false;
+        }
+        GroupAuthorities other = (GroupAuthorities) object;
+        if ((this.groupAuthoritiesPK == null && other.groupAuthoritiesPK != null) || (this.groupAuthoritiesPK != null && !this.groupAuthoritiesPK.equals(other.groupAuthoritiesPK))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.example.springboot.demospringboot.model.GroupAuthorities[ groupAuthoritiesPK=" + groupAuthoritiesPK + " ]";
+    }
+    
+}
